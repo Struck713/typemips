9549971be0b4fbd7165a830cac1e734259e6fe3a
Struck713 <noahstruck71303@gmail.com>
2023-10-19 02:55:46 -0400

    Added a primative parser and assembler

---
+++ src/assembler.ts
@@ -0,0 +1,102 @@
+import { parse } from "path";
+import { Instructions, Token, TokenType } from "./tokenizer";
+import { env } from "process";
+
+class Assembler {
+
+    private cursor: number;
+    private tokens: Token[];
+    private environment: { 
+        [key: string]: (string | number) 
+    };
+
+    constructor(tokens: Token[]) {
+        this.cursor = 0;
+        this.tokens = tokens;
+        this.environment = {};
+    }
+
+    assemble = () => {
+        const instructions = Object.values(Instructions);
+        while (this.cursor < this.tokens.length) {
+            let { type, value } = this.next();
+            if (type == TokenType.IDENTIFIER) {
+                if (instructions.includes(value as Instructions)) this.assemble_instruction(value as Instructions);
+                else if (value === "syscall") this.assemble_syscall();
+                else if (this.peek().type == TokenType.COLON) this.assemble_label();
+            }
+        }
+    }
+
+    assemble_instruction = (instruction: Instructions) => {
+        let register;
+        switch (instruction) {
+            case Instructions.LOAD_IMMEDIATE:
+                register = this.parse_register();
+                this.cursor++; // skip the comma
+                let immediate = this.parse_immediate();
+                this.environment[register] = immediate;
+                break;
+            case Instructions.LOAD_ADDRESS:
+                register = this.parse_register();
+                this.cursor++; // skip the comma
+                let label = this.parse_address();
+                this.environment[register] = label;
+                break;
+        }
+    }
+
+    assemble_label = () => {
+        let label = this.peekLast();
+        if (label.type === TokenType.IDENTIFIER) {
+            this.next(); // skip the colon
+            let next = this.peek();
+            if (next.type === TokenType.IDENTIFIER) { // modify the environment (or not)
+                if (next.value == "asciiz") {
+                    this.next();
+                    let string = this.peek();
+                    if (string.type == TokenType.STRING) this.environment[label.value] = string.value;
+                    else throw new Error(`Expected string, got ${string.value}`);
+                }
+                // do nothing, we only need to register the asciiz label
+            }
+        };
+    }
+
+    assemble_syscall = () => {
+        let type = this.environment["$v0"];
+        if (type === 4) {
+            let address = this.environment["$a0"];
+            let string = this.environment[address];
+            console.log(string);
+        }
+    }
+
+    parse_register = () => {
+        let { type, value } = this.next();
+        if (type == TokenType.REGISTER) return value;
+        else throw new Error(`Expected register, got ${value}`);
+    }
+    
+    parse_immediate = () => {
+        let { type, value } = this.next();
+        if (type == TokenType.IMMEDIATE) return Number(value);
+        else throw new Error(`Expected immediate value, got ${value}`);
+    }
+
+    parse_address = () => {
+        let { type, value } = this.next();
+        if (type == TokenType.IDENTIFIER) return value;
+        else throw new Error(`Expected address value, got ${value}`);
+    }
+
+    next = () => this.tokens[this.cursor++];
+    peek = () => this.tokens[this.cursor];
+    peekLast = () => this.tokens[this.cursor - 1] ?? { type: TokenType.EOF, value: "EOF" };
+
+}
+
+export const assemble = (tokens: Token[]) => {
+    const assembler: Assembler = new Assembler(tokens);
+    return assembler.assemble();
+}
